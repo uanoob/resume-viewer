1bf7fb7c3cb13517b2f3390f6e9db8af3b133b67
uanoob <usnet@ukr.net>
2018-01-22 17:20:45 +0200

    Template & Pages

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,9 +3,26 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return (
-      <div >
-        Footer
-      </div>
+      <footer>
+        <div className="row">
+          <div className="twelve columns">
+            <ul className="copyright">
+              <li>&copy; Copyright 2014 CeeVee</li>
+              <li>
+                Design by{' '}
+                <a title="Styleshout" href="http://www.styleshout.com/">
+                  Styleshout
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div id="go-top">
+            <a className="smoothscroll" title="Back to Top" href="#home">
+              <i className="icon-up-open" />
+            </a>
+          </div>
+        </div>
+      </footer>
     );
   }
 }

5f961ca66ab3cd9fd4671b5c3178d0ac0012f79e
uanoob <usnet@ukr.net>
2018-01-22 15:40:50 +0200

    Create Components

---
+++ src/Components/Testimonials.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Testimonials extends Component {
+  render() {
+    return (
+      <div >
+        Testimonials
+      </div>
+    );
+  }
+}
+
+export default Testimonials;
